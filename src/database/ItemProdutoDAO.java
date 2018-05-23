4d30aadb1c1c259f738912bc8653f7f5a3c9e787
jennicg <37018681+jennicg@users.noreply.github.com>
2018-05-23 17:05:15 -0300

    readProdutosBySupermercado

--- src/database/ItemProdutoDAO.java
+++ src/database/ItemProdutoDAO.java
@@ -5,14 +5,18 @@
  */
 package database;
 
+import static database.DBCommand.getConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import modelo.ItemProduto;
 import modelo.cliente.Compra;
+import modelo.supermercado.mercadoria.Produto;
 
 /**
  *
@@ -51,8 +55,49 @@ public abstract class ItemProdutoDAO extends DBCommand{
     }
     
     //Jennifer
-    public static List<ItemProduto> readItensByCompra(Compra compra){
+    public static List<ItemProduto> readItensByCompra(Compra compra)throws SQLException, ClassNotFoundException{
         //Faça uso da função ProdutoDAO.readProdutoById(int id)
         //Servira para pegar o produto do item em questao
+        
+        List<ItemProduto> itens = new ArrayList<>();
+
+        // Obtenha a conexão com o BD;
+        Connection conexao = getConnection();
+ 
+        // Forme a string sql;
+        String sql = "SELECT * from produto "
+               + "INNER JOIN produto on produto.id = compra.id"
+                + "WHERE id = ?"; //REVER SE ESTA CERTO
+        //int id, double precoCompra, int quantidade, Produto produto
+        PreparedStatement st = conexao.prepareStatement (sql);
+        st.setInt(1, compra.getId());
+        
+        ResultSet rs = st.executeQuery();
+ 
+        while (rs.next()) {
+  
+            int id = rs.getInt("id");
+            double precoCompra = rs.getDouble("preco_compra");
+            int quantidade = rs.getInt("quantidade");
+            //String codigo, double custo, String descricao, String marca, String nome, double precoVenda, int qtdPrateleira, int qtdEstoque, String tipo
+            String codigo = rs.getString("codigo");
+            double custo = rs.getDouble("custo");
+            String descricao = rs.getString("descricao");
+            String marca = rs.getString("marca");
+            String nome = rs.getString("nome");
+            double preco = rs.getDouble("preco");
+            int quant_prateleira = rs.getInt("quant_prateleira");
+            int estoque = rs.getInt("estoque");
+            String tipo = rs.getString("tipo");
+            Produto prod = new Produto(codigo,custo,descricao,marca,nome,preco,quant_prateleira,estoque,tipo);
+            
+             
+            itens.add(new ItemProduto(id,precoCompra,quantidade,prod));
+        }
+
+        st.close();
+        conexao.close();
+
+        return itens;
     }
 }
