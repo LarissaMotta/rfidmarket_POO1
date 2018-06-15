86dd46b93280a31cbcc8ddd21cdc4db8ee05570f
LarissaMotta <37023089+LarissaMotta@users.noreply.github.com>
2018-06-14 23:44:25 -0300

    Falta by(fornecedor,Lote e Produto)

--- test/database/supermercado/SupermercadoDAOTest.java
+++ test/database/supermercado/SupermercadoDAOTest.java
@@ -6,16 +6,28 @@
 package database.supermercado;
 
 import controlTest.ResetTable;
+import database.pagamento.CartaoDAO;
+import database.supermercado.mercadoria.ProdutoDAO;
+import database.usuarios.ClienteDAO;
+import database.usuarios.FuncionarioDAO;
 import database.usuarios.PessoaJuridicaDAO;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import modelo.pagamento.Cartao;
 import modelo.supermercado.Compra;
 import modelo.supermercado.Supermercado;
 import modelo.supermercado.mercadoria.Fornecedor;
+import modelo.supermercado.mercadoria.ItemProduto;
 import modelo.supermercado.mercadoria.Lote;
 import modelo.supermercado.mercadoria.Produto;
+import modelo.usuarios.Cliente;
 import modelo.usuarios.Endereco;
 import modelo.usuarios.Funcionario;
+import objGeneretor.CartaoTDAO;
+import objGeneretor.ClienteTDAO;
+import objGeneretor.FuncionarioTDAO;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -60,40 +72,60 @@ public class SupermercadoDAOTest {
         ResetTable.cleanAllTables();
     }
     
-    @Test
+    /*@Test
     public void testDelete() throws Exception {
         System.out.println("delete");
         int id = supermercado.getId();
         PessoaJuridicaDAO.delete(id);
-    }
+    }*/
 
 
     /**
      * Test of readSupermercadoByFuncionario method, of class SupermercadoDAO.
+     * @throws java.lang.Exception
      */
     @Test
     public void testReadSupermercadoByFuncionario() throws Exception {
         System.out.println("readSupermercadoByFuncionario");
-        Funcionario funcionario = null;
-        Supermercado expResult = null;
+        
+        Endereco endereco = new Endereco("Jacaraípe", "29177-486", "SERRA", Endereco.Estado.ES, 75, "Rua Xablau");
+        Supermercado superm = new Supermercado(18.5774, 15.1741, "Serra", "35.415.363/0001-72", "Carone", endereco);
+        int idSuperm = SupermercadoDAO.create(superm);
+        superm = new Supermercado(idSuperm, superm.getLatitude(), superm.getLongitude(), superm.getUnidade(), superm.getCnpj(), superm.getNome(), endereco);
+        
+        Funcionario funcionario = FuncionarioTDAO.readFuncionario();
+        FuncionarioDAO.create(funcionario, supermercado);
+        //Funcionario funcionario
         Supermercado result = SupermercadoDAO.readSupermercadoByFuncionario(funcionario);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println(result);
     }
 
+    
+    
+    
     /**
      * Test of readSupermercadoByCompra method, of class SupermercadoDAO.
      */
     @Test
     public void testReadSupermercadoByCompra() throws Exception {
         System.out.println("readSupermercadoByCompra");
-        Compra compra = null;
-        Supermercado expResult = null;
+        
+        Endereco endereco = new Endereco("Jacaraípe", "29177-486", "SERRA", Endereco.Estado.ES, 75, "Rua Xablau");
+        Supermercado superm = new Supermercado(18.5774, 15.1741, "Serra", "35.415.363/0001-72", "Carone", endereco);
+        int idSuperm = SupermercadoDAO.create(superm);
+        superm = new Supermercado(idSuperm, superm.getLatitude(), superm.getLongitude(), superm.getUnidade(), superm.getCnpj(), superm.getNome(), endereco);
+        List<ItemProduto> itens = new ArrayList<>();
+        Cliente cliente = ClienteTDAO.readCliente();
+        Cartao cartao = CartaoTDAO.readCartao();
+        Produto produto = new Produto("0000", 20.00,"Premium care", "Pampers","Fralda XG", 35.00, 30, 40, "fralda");
+        ItemProduto itemProduto = new ItemProduto(35.00,02,produto);
+        itens.add(itemProduto);
+        Compra compra = new Compra(new Date(14,06,2018),itens);
+        CompraDAO.create(compra,cliente,cartao,supermercado);
+
         Supermercado result = SupermercadoDAO.readSupermercadoByCompra(compra);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println(result);
+
     }
 
     /**
