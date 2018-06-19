6a3c7d0095eab5a146cf67ba01fa8ba92a224254
joelwb <37381440+joelwb@users.noreply.github.com>
2018-06-19 16:51:19 -0300

    Adição da função que retorna o uso de cada tipo de cartão em cada dia

--- src/database/pagamento/CartaoDAO.java
+++ src/database/pagamento/CartaoDAO.java
@@ -2,6 +2,8 @@ package database.pagamento;
 
 import database.core.CoreDAO;
 import static database.core.CoreDAO.getConnection;
+import database.filter.Clause;
+import database.filter.Filter;
 
 import java.sql.Connection;
 import java.sql.Date;
@@ -12,10 +14,13 @@ import java.sql.Statement;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import modelo.pagamento.Cartao;
 import modelo.pagamento.Cartao.Tipo;
+import modelo.supermercado.Supermercado;
 import modelo.usuarios.Cliente;
 
 public abstract class CartaoDAO extends CoreDAO {
@@ -26,11 +31,11 @@ public abstract class CartaoDAO extends CoreDAO {
         Connection conexao = getConnection();
 
         // Forme a string básica de sql;
-        String sql = "INSERT INTO cartao" +
-                "(nome_titular, validade, bandeira, numero, tipo)" +
-                " VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO cartao"
+                + "(nome_titular, validade, bandeira, numero, tipo)"
+                + " VALUES (?, ?, ?, ?, ?)";
 
-        PreparedStatement st = conexao.prepareStatement (sql, Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement st = conexao.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
         // Defina os valores que ocuparão as '?' na ordem acima;
         st.setString(1, cartao.getTitular());
@@ -49,11 +54,9 @@ public abstract class CartaoDAO extends CoreDAO {
         return id;
     }
 
-  
-    
-   
     /**
      * Retorna um conjunto de cartões relacionados a um cliente;
+     *
      * @param cliente cliente usuário do(s) cartão(ões);
      * @return Conjunto de cartões relacionados ao cliente;
      * @throws java.sql.SQLException
@@ -67,13 +70,13 @@ public abstract class CartaoDAO extends CoreDAO {
         Connection conexao = getConnection();
 
         // Forme a string sql;
-        String sql = "SELECT c.id, c.nome_titular, c.validade, c.bandeira, c.numero," +
-                "c.tipo FROM utiliza AS ut INNER JOIN cartao AS c" +
-                "ON ut.fk_cartao = c.id WHERE ut.fk_cliente = ?";
+        String sql = "SELECT c.id, c.nome_titular, c.validade, c.bandeira, c.numero,"
+                + "c.tipo FROM utiliza AS ut INNER JOIN cartao AS c"
+                + "ON ut.fk_cartao = c.id WHERE ut.fk_cliente = ?";
 
-        PreparedStatement st = conexao.prepareStatement (sql);
+        PreparedStatement st = conexao.prepareStatement(sql);
         st.setInt(1, cliente.getId());
-        
+
         ResultSet rs = st.executeQuery();
 
         // Enquanto houver algum cartão resultado da busca;
@@ -85,10 +88,13 @@ public abstract class CartaoDAO extends CoreDAO {
             String numero = rs.getString("numero");
             String titular = rs.getString("nome_titular");
             char tipo = rs.getString("tipo").charAt(0);
-            
+
             Tipo type;
-            if (tipo == 'C') type = Tipo.CREDITO;
-            else type = Tipo.DEBITO;
+            if (tipo == 'C') {
+                type = Tipo.CREDITO;
+            } else {
+                type = Tipo.DEBITO;
+            }
 
             cartoes.add(new Cartao(id, bandeira, dataValid, numero, titular, type));
         }
@@ -98,49 +104,60 @@ public abstract class CartaoDAO extends CoreDAO {
 
         return cartoes;
     }
+
+    public static Map<String, Map<java.util.Date, Integer>> getMeiosPagMaisUsado(Supermercado supermercado, java.util.Date dataMin, java.util.Date dataMax) throws ClassNotFoundException, SQLException {
+        // criacao do hashmap
+        Map<String, Map<java.util.Date, Integer>> map = new HashMap<>();
+        map.put("Crédito", new LinkedHashMap<>());
+        map.put("Débito", new LinkedHashMap<>());
+
+        Connection con = getConnection();
+
+        Filter filter = new Filter();
+
+        Clause clause = new Clause("hc.timestamp", dataMin, Clause.MAIOR_IGUAL);
+        filter.addClause(clause);
+
+        clause = new Clause("hc.timestamp", dataMax, Clause.MENOR_IGUAL);
+        filter.addClause(clause);
+
+        String sql = "SELECT c.tipo, DATE(timestamp) as data_compra, COUNT(c.tipo) as num_uso FROM hist_compra AS hc "
+                + "INNER JOIN cartao as c ON hc.fk_cartao = c.id WHERE hc.fk_supermercado = ? " + filter.getFilter()
+                + " GROUP BY (c.tipo,data_compra) ORDER BY data_compra";
+
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1, supermercado.getId());
+        
+        ResultSet rs;
+        try {
+            rs = st.executeQuery();
+        } catch (SQLException ex) {
+            con.close();
+            throw ex;
+        }
+
+        while (rs.next()) {
+
+            String tipo = rs.getString("tipo");
+            java.util.Date dataCompra = new java.util.Date(rs.getDate("data_compra").getTime());
+            int numUsos = rs.getInt("num_uso");
+
+            if (tipo.equals("C")) {
+                map.get("Crédito").put(dataCompra, numUsos);
+            } else {
+                map.get("Débito").put(dataCompra, numUsos);
+            }
+        }
+
+        st.close();
+        con.close();
+
+        return map;
+    }
 }
 
-/*
-  public static void main(String args[]) {
-      //-- Qual o meio de pagamento mais utilizado?
-        // criacao do hashmap
-        HashMap<Cartao, String> map3 = new HashMap<Cartao, String>();
-        
-        Statement st = null;
-        ResultSet rs = null;
-        Connection con = getConnection();
-        Cartao card;
-        //int id, String bandeira, Date dataValid, String numero, String titular, Tipo tipo) 
-        try{
-            st = con.createStatement();
-            rs = st.executeQuery("SELECT c.tipo "Tipo de cartão", COUNT(c.tipo) "Numero de usuários" FROM hist_compra AS hc INNER JOIN cartao c ON hc.id = c.id WHERE hc.fk_supermercado = ? AND hc.timestamp >= ? AND hc.timestamp <= ? GROUP BY (c.tipo)");
-            while(rs.next()){
-               //nome,preco,codigo,descricao,custo,id,estoque, tipo, quant_pratelereira, marca,fk_supermercado
-                
-                int id = rs.getInt("id");
-                Date dataValid = rs.getDate("validade");
-                String bandeira = rs.getString("bandeira");
-                String numero = rs.getString("numero");
-                String titular = rs.getString("titular");
-                String tipo = rs.getString("tipo");
-                //int id, String codigo, double custo, String descricao, String marca, String nome, double precoVenda, int qtdPrateleira, int qtdEstoque, String tipo)
-                
-                card = new Cartao(id,dataValid,bandeira,numero,titular,Tipo.tipo);
-                
-         
-                map3.put(card,tipo);
-            }
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }
-        
-   
-        }
 
- 
-
-
-    public static void main(String args[]) {
+/*public static void main(String args[]) {
      // --Quais meio de pagamento mais lucrativo
         // criacao do hashmap
         HashMap<Cartao, String> map4 = new HashMap<Cartao, String>();
@@ -177,6 +194,4 @@ public abstract class CartaoDAO extends CoreDAO {
         }
 
 
-*/
-
-
+ */
