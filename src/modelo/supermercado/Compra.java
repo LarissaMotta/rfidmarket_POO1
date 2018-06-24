d3caad3ed56e5b94a09820dfb65781d3c80e7abe
joelwb <37381440+joelwb@users.noreply.github.com>
2018-06-24 10:22:04 -0300

    correção no calculo do valor total da compra

--- src/modelo/supermercado/Compra.java
+++ src/modelo/supermercado/Compra.java
@@ -69,7 +69,7 @@ public class Compra {
         
         double valor = 0;
         for (ItemProduto item : itens){
-            valor += item.getPrecoCompra();
+            valor += item.getPrecoCompra() * item.getQuantidade();
         }
         
         return valor;
