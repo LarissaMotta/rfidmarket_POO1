c2bafe43e9815582c30e0f093c312282c6671934
joelwb <37381440+joelwb@users.noreply.github.com>
2018-06-27 17:55:58 -0300

    Testes ClienteDAO finalizado

--- test/database/usuarios/ClienteDAOTest.java
+++ test/database/usuarios/ClienteDAOTest.java
@@ -20,6 +20,7 @@ import java.util.List;
 import database.supermercado.CompraDAO;
 import database.supermercado.mercadoria.ProdutoDAO;
 import database.supermercado.SupermercadoDAO;
+import java.util.Map;
 import modelo.supermercado.mercadoria.ItemProduto;
 import modelo.pagamento.Cartao;
 import modelo.usuarios.Cliente;
@@ -41,6 +42,7 @@ import static org.junit.Assert.*;
  */
 public class ClienteDAOTest {
     private Cliente cliente;
+    private Supermercado supermercado;
     
     public ClienteDAOTest() {
     }
@@ -59,11 +61,18 @@ public class ClienteDAOTest {
         System.out.println("create");
         
         Endereco endereco = new Endereco("Jacaraípe", "29177-486", "SERRA", Endereco.Estado.ES, 75, "Rua Xablau");
-        cliente = new Cliente("216.856.707-76", new Date(), PessoaFisica.Genero.M, "joel@hotmail.com", "testedesenha", "Joel", endereco);
-        int result = ClienteDAO.create(cliente);
-        cliente = new Cliente(cliente.getCpf(), cliente.getDataNasc(), cliente.getGenero(), cliente.getLogin(), cliente.getSenha(),result, cliente.getNome(), endereco);
         
-        System.out.println("id = "+result);
+        cliente = new Cliente("216.856.707-76", new Date(), PessoaFisica.Genero.M, "joel@hotmail.com", "testedesenha", "Joel", endereco);
+        int idCliente = ClienteDAO.create(cliente);
+        
+        cliente = new Cliente(cliente.getCpf(), cliente.getDataNasc(), cliente.getGenero(), cliente.getLogin(), cliente.getSenha(),idCliente, cliente.getNome(), endereco);
+        System.out.println("idCliente = "+idCliente);
+        
+        supermercado = new Supermercado(-52.2471,-2.5297,"serra 03","44.122.623/0001-02", "EPA", endereco);
+        int idSupermercado = SupermercadoDAO.create(supermercado);
+        
+        supermercado = new Supermercado(idSupermercado,supermercado.getLatitude(),supermercado.getLongitude(),supermercado.getUnidade(),supermercado.getCnpj(), supermercado.getNome(), endereco);
+        System.out.println("idSupermercado = "+idSupermercado);
     }
     
     @After
@@ -78,11 +87,12 @@ public class ClienteDAOTest {
     public void testAddCartao() throws Exception {
         System.out.println("addCartao");
         
-        Cartao cartao = new Cartao("MasterCard", new  Date(2019, 8, 1), "5482657412589634", "Maria", Cartao.Tipo.CREDITO);
-        int id = CartaoDAO.create(cartao);
+        Cartao cartao = new Cartao("MasterCard", new Date("2018/05/28"), "5482657412589634", "Maria", Cartao.Tipo.CREDITO);
+        int idCartao = CartaoDAO.create(cartao);
         
-        cartao = new Cartao(id, cartao.getBandeira(), cartao.getDataValid(), cartao.getNumero(), cartao.getTitular(), cartao.getTipo());
+        cartao = new Cartao(idCartao, cartao.getBandeira(), cartao.getDataValid(), cartao.getNumero(), cartao.getTitular(), cartao.getTipo());
         
+        System.out.println("idCartao = "+idCartao);
         ClienteDAO.addCartao(cliente, cartao);
     }
 
@@ -120,5 +130,26 @@ public class ClienteDAOTest {
         
         List<Cliente> result = ClienteDAO.readClientesBySupermercado(superm,null,null,null);
         System.out.println(result);//pus pra tirar o erro do "result"
+    }
+
+
+    /**
+     * Test of readClientesMaisConsumistas method, of class ClienteDAO.
+     */
+    @Test
+    public void testReadClientesMaisConsumistas() throws Exception {
+        System.out.println("readClientesMaisConsumistas");
+        Map<Cliente, Double> result = ClienteDAO.readClientesMaisConsumistas(supermercado, null, null, 100);
+        assertEquals(0, result.size());
+    }
+
+    /**
+     * Test of readClientesMediaConsumo method, of class ClienteDAO.
+     */
+    @Test
+    public void testReadClientesMediaConsumo() throws Exception {
+        System.out.println("readClientesMediaConsumo");
+        Map<Cliente, Double> result = ClienteDAO.readClientesMediaConsumo(supermercado, null, null, 100);
+        assertEquals(0, result.size());
     }
 }
